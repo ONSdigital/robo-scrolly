8e2f2b9c6cacc48b1014d164675712b903e95c82
Ahmad Barclay <ahmad.barclay@ons.gov.uk>
2023-04-26 14:06:11 +0100

    small amendment to layercake fix

--- scripts/layercake-fix.js
+++ scripts/layercake-fix.js
@@ -3,7 +3,9 @@ import { readFileSync, writeFileSync } from "fs";
 const path = "./node_modules/layercake/package.json";
 
 const json = JSON.parse(readFileSync(path, {encoding:'utf8', flag:'r'}));
-json.exports = {".": "./dist/index.js"};
 
-writeFileSync(path, JSON.stringify(json, null, 2));
-console.log("Fixed layercake package.json");
+if (json?.exports?.["."] !== "./dist/index.js") {
+  json.exports = {".": "./dist/index.js"};
+  writeFileSync(path, JSON.stringify(json, null, 2));
+  console.log("Fixed layercake package.json");
+}
